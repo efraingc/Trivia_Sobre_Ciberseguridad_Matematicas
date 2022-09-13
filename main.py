2eb10ca8bc60f83aeac0c3e7d73f6eb7e160e31a
Efrain Garcia <113326829+efraingc@users.noreply.github.com>
2022-09-13 21:45:23 +0000

    Add, some new features and details to the game.

--- main.py
+++ main.py
@@ -1,39 +1,43 @@
-#Importamos la libreria ramdom
+
 import random
-#Para implementar puntajes, crearemos una nueva vaiable llamada 'puntaje', la que inicializamos en 0.
+import time
+
 puntaje = random.randint(0, 10)
 
-#Para implementar puntajes, crearemos una nueva vaiable llamada 'puntaje', la que inicializamos en 0.
-
+iniciar_trivia = True 
+intentos = 0
 puntaje = 0
-#Agregamos perzonalizacion para nuestros jugadores, pregutando y almacenado sus nombres en una bariable
-nombre = input('Ingresa tu nombre: ')
+
+print("\033[34m Bienvenido a trivia sobre ciberseguridad \033[39m")
+print(" \033[34m Pondremos a prueba tus conocimientos \033[39m")
+print('Inicias con', puntaje, 'puntos')
+
+BLACK = '\033[30m'
+RED = '\033[310m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[38m'
+
+nombre = input(GREEN+'\nIngresa tu nombre: '+RESET)
+
+print(BLUE+'\n Hola', nombre, "Responder las siguientes preguntas escribiendo la letra de la alternativa y presionando 'Enter' para enviar tu respuesta:\n"+RESET)
+print ('Hola', nombre, 'iniciamos en 3 segundos...')
+time.sleep(3)
+print('Lindo nombre', nombre)
 
 
-# Lo primero es mostrar en pantalla el texto de bienvenida para quien juegue tu trivia
 
-print("Bienvenido a trivia sobre ciberseguridad")
-print("Pondremos a prueba tus conocimientos")
-print('Tienes', puntaje, 'puntos')
-
-#Agregamos perzonalizacion para nuestros jugadores, pregutando y almacenado sus nombres en una variable
-nombre = input('Ingresa tu nombre: ')
-
-# Es importante dar instrucciones sobre cómo jugar
-# Ahora, lo personalizaremos con el nombre del jugador, diciendole a print() que muestre el contenido de la variable 'nombre'.  Cada cosa distinta que queremos que nos muestre en la pantlla, la separamos con comas
-print('\n Hola', nombre, "Responder las siguientes preguntas escribiendo la letra de la alternativa y presionando 'Enter' para enviar tu respuesta:\n"
-)
-
-# OJO, el \n al final de la línea 6 es para dar un "salto de línea"
-
-# Pregunta 1
 print("1) ¿Que es es un ataque DDoS?")
-print("a) un ataque de denegación")
+print(YELLOW+"a) un ataque de denegación")
 print("b) ataque distribuido de denegación de servicio")
 print("c) una versión distribuida")
-print("d) un grupo de personas o automatismos atacan a un servidor")
+print("d) un grupo de personas o automatismos atacan a un servidor"+RESET)
 
-# Almacenamos la respuesta del usuario en la variable "respuesta_1"
+
 respuesta_1 = input("\nTu respuesta: ")
 if respuesta_1 == 'x':
   print('Nunca te rindas!')
@@ -59,14 +63,14 @@ else:
 
   
   
-#Pregunta 2
+
 print("\n¿Que es Kali Linux?")
 print("a) Un sistema Operativo basado en Debian")
 print("b) Un sistema Operativo usado por Pentesting")
 print("c) Un Browser de linux")
 print("d) Una distribucion basada en debian GNU/Linux")
 print("e) Un Kernel")
-#Almacenamos la respuesta usuario en la variable respuesta_2
+
 respuesta_2 = input("\nTu Respuesta: ")
 if respuesta_2 == 'x':
   print('Nunca te rindas!')
@@ -90,12 +94,13 @@ else:
   puntaje += 10
   puntaje = random.randint(0, 10)
   print('Muy bien!', nombre, 'tienes', puntaje,  'puntos!')
-#Pregunta 3
+
+print('Iniciamos a jugar con los numeros')
 print('\nHola', nombre, "Puedes ralizar tus operaciones aqui:\n"
 )
 
-num1 = int(input('Ingrese el numero'))
-num2 = int(input('Ingrese el numero: '))
+num1 = int(input('Ingrese el 1 numero: '))
+num2 = int(input('Ingrese el 2 numero: '))
 op = input('Ingrese la operacion: ')
 if op == '+':
   print('Resultado: ', num1+num2)
@@ -105,21 +110,21 @@ elif op == '*':
   print('Rsultado: ', num1*num2)
 elif op == '/':
   print('Resultado: ', num1/num2)
-#Pregunta 4
+
 def ejercicio1():
-  x = int(input("Ingrese el primer numero: "))
+  x = int(input("\nIngrese el primer numero: "))
   y = int(input("Ingrese el segundo numero: "))
   print((x+y)/2)
-  #Pregunta 5
+ 
 def ejercicio2():
   x = int(input("Ingrese el primer numero: "))
   y = int(input("Ingrese el segundo numero: "))
   print(x**y)
-#Pregunta 6
+
 def ejercicio3():
   x = int(input("Ingrese el numero: "))
   print(x**(1/2))
-#Ejercicio 7
+
 def ejercicio4():
   x = int(input("Ingrese el primer numero: "))
   y = int(input("Ingrese el segundo numero: "))
@@ -134,6 +139,46 @@ elif num == 3:
   ejercicio3()
 elif num == 4:
   ejercicio4()
+
+x = int(input('Ingrese el numero: '))
+x = x * x
+if x >= 0:
+  print('El numero es positvo')
+else:
+  print('El numero es negativo')
+
+x = int(input('Primer numero: '))
+y = int(input('Segundo numero: '))
+if x > y:
+  print('El numero', x, 'es mayor')
+else:
+  print('El numero', y, 'es mayor')
+
+print(GEEN+'\nJuega un momento con los numeros', nombre+RESET)
+z = input('\nIngresa la palabra: ')
+x = int(input('Numero de veces: '))
+y = 0
+while y < x:
+  print(z)
+  y = y + 15
+
+print('Jugemnos un momento con las filas de sginos, formados figuras ')
+x = int(input('numero de filas: '))
+y = 0
+while y <= x:
+  z = 0
+  while z < y:
+    print('+', end = '')
+    z = z + 1
+  print('')  
+  y = y + 1
   
 
-  print('Gracias!', nombre, 'por jugar mi trivia, alcanzaste', puntaje, 'puntos')
+
+print('\n¿Deseas intentar nuevamente mi Trivia?')
+repetir_trivia = input('Ingresa <si> para repetir, o cualquier tecla para finalizar: ').lower()
+
+if repetir_trivia != 'si':
+  print('\nEspero {nombre} que lo hayas pasdo bien, hsta pronto!')
+
+print('Gracias!', nombre, 'por jugar mi trivia, alcanzaste', puntaje, 'puntos')
